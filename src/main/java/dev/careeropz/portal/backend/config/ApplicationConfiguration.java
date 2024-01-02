f681b0c779dedd9ca8e4ff141b2e7089cca5ec75
isuru <isurucuma@gmail.com>
2024-01-03 00:32:36 +0530

    Cors configuration added

--- src/main/java/dev/careeropz/portal/backend/config/ApplicationConfiguration.java
+++ src/main/java/dev/careeropz/portal/backend/config/ApplicationConfiguration.java
@@ -3,6 +3,10 @@ package dev.careeropz.portal.backend.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -10,5 +14,26 @@ public class ApplicationConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name()
+                        )
+                        .allowedHeaders(
+                                HttpHeaders.CONTENT_TYPE,
+                                HttpHeaders.AUTHORIZATION);
+            }
+        };
     }
 }
