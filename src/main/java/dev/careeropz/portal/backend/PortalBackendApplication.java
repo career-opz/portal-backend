9fdd5f7c325049c9ecd9bf7e2624129899360cfd
isuru <isurucuma@gmail.com>
2024-02-18 19:41:47 +0530

    keycloak integrated, docker files updated

--- src/main/java/dev/careeropz/portal/backend/PortalBackendApplication.java
+++ src/main/java/dev/careeropz/portal/backend/PortalBackendApplication.java
@@ -1,5 +1,7 @@
 package dev.careeropz.portal.backend;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
--- src/main/java/dev/careeropz/portal/backend/config/OpenApiConfig.java
+++ src/main/java/dev/careeropz/portal/backend/config/OpenApiConfig.java
@@ -21,11 +21,12 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 )
 @SecurityScheme(
         name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
+        type = SecuritySchemeType.OPENIDCONNECT,
         description = "Authentication needed to access the APIs",
         scheme = "bearer",
         bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
+        in = SecuritySchemeIn.HEADER,
+        openIdConnectUrl = "http://localhost:7070/realms/cv-manager/.well-known/openid-configuration"
 )
 public class OpenApiConfig {
 }
